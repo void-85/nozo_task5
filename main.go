9e5f90e19eb1274b397abda17b6dbfbf31439c7d
void-85 <zGonzo@doctor.com>
2025-07-16 00:40:24 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+const (
+	test_dir = "test"
+)
+
+func main() {
+
+	fmt.Printf("\n\033[33m[ STARTED ]\n")
+
+	test_input_files, err := filepath.Glob(filepath.Join(test_dir, "*"))
+	if err != nil {
+		fmt.Printf("\033[31mfailed to list input files: %v", err)
+	}
+
+	for _, in_file := range test_input_files {
+
+		file_info, err := os.Stat(in_file)
+		if err != nil || !file_info.Mode().IsRegular() || strings.Contains(file_info.Name(), ".a") {
+			continue
+		}
+
+		fmt.Printf("\033[37mtesting file \"%s\"\n", in_file)
+		out_file := in_file + ".a"
+
+		input, err := os.ReadFile(in_file)
+		if err != nil {
+			fmt.Printf("\033[31mfailed to read input file %s: %v\n", in_file, err)
+			continue
+		}
+
+		expectedOutput, err := os.ReadFile(out_file)
+		if err != nil {
+			fmt.Printf("\033[31mfailed to read output file %s: %v\n", out_file, err)
+			continue
+		}
+
+		origStdin := os.Stdin
+		origStdout := os.Stdout
+		rIn, wIn, _ := os.Pipe()
+		wIn.Write(input)
+		wIn.Close()
+		os.Stdin = rIn
+		rOut, wOut, _ := os.Pipe()
+		os.Stdout = wOut
+		start := time.Now()
+		test_func()
+		wOut.Close()
+		duration := time.Since(start)
+		var buf bytes.Buffer
+		io.Copy(&buf, rOut)
+		os.Stdin = origStdin
+		os.Stdout = origStdout
+
+		actualOutput := strings.TrimSpace(buf.String())
+		expected := strings.TrimSpace(string(expectedOutput))
+
+		if actualOutput != expected {
+			fmt.Printf("\n\033[31mFAILED %s (worked %s)\nExpected:\n%s\nGot:\n%s\n", in_file, duration, expected, actualOutput)
+		} else {
+			fmt.Printf("\033[32mPASSED %s 	(worked %s)\n", in_file, duration)
+		}
+
+	}
+
+	fmt.Printf("\033[33m[ FINISHED ]\n\n")
+}
+
+func test_func() {
+	inp := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var datasets int
+	fmt.Fscan(inp, &datasets)
+
+	log.Printf("\033[35m TRYING %d DATASETS\n", datasets)
+
+	fmt.Fprint(out, "")
+}
